5742620a8c80288a46d9110273c379d6fbce9b01
Luca Hodge <luho3568@colorado.edu>
2023-11-02 11:04:59 -0600

    day 5 part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -95,6 +95,23 @@ fn perform_all_moves (stacks : &mut Vec<Vec<char>>, moves: Vec<(i32,i32,i32)>) {
     }
 }
 
+fn perform_move_2 (stacks : &mut Vec<Vec<char>>, m: (i32,i32,i32)) {
+    let mut my_crates = vec!();
+    for _ in 0..m.0 {
+        my_crates.push(stacks[m.1 as usize].pop().unwrap());
+    }
+    for my_crate in my_crates.into_iter().rev() {
+        stacks[m.2 as usize].push(my_crate);
+    }
+}
+
+fn perform_all_moves_2 (stacks : &mut Vec<Vec<char>>, moves: Vec<(i32,i32,i32)>) {
+    for m in moves {
+        perform_move_2(stacks, m);
+        // println!("In all moves: {:?}", stacks);
+    }
+}
+
 fn get_top_crates (stacks : &Vec<Vec<char>>) -> String {
     let mut s : String = String::new();
     for stack in stacks {
@@ -122,9 +139,28 @@ fn part1 () {
     // println!("Final Stack: {:?}", stacks);
     
     //get the top list
-    println!("Top list : {}", get_top_crates(&stacks));
+    println!("Top list 1: {}", get_top_crates(&stacks));
+}
+
+fn part2 () {
+    let lines = file_to_strings("files/crate_stack.txt".to_string());
+    // let lines = file_to_strings("files/crate_stack_test.txt".to_string());
+    let (diagram_lines, move_lines) = split_on_empty(lines);
+
+    //create the stacks
+    let mut stacks = diagram_to_stacks(diagram_lines);
+
+    //create the lists of operations
+    let moves = parse_moves(move_lines);
+
+    //operate on stacks
+    perform_all_moves_2(&mut stacks, moves);
+    
+    //get the top list
+    println!("Top list 2: {}", get_top_crates(&stacks));
 }
 
 fn main() {
     part1();
+    part2();
 }
