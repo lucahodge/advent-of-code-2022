4ca25eff3383c423832fbfd1e86b96741a64ec3f
Luca Hodge <lucahodge@gmail.com>
2023-11-08 09:36:33 -0700

    day 9 part 1

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -13,7 +13,9 @@ fn file_to_moves (file_name : String) -> Vec<(Move, i32)> {
     let lines : Vec<String> = fs::read_to_string(file_name).unwrap().lines().map(|x| x.to_string()).collect();
     for line in lines {
         let chars : Vec<char> = line.chars().collect();
-        let num : i32 = chars[2] as i32 - '0' as i32;
+        // let num : i32 = chars[2] as i32 - '0' as i32;
+        let num_str : String = line.chars().skip(2).collect();
+        let num : i32 = num_str.parse().unwrap();
         let move_option = match chars[0] {
             'U' => Some(Move::Up),
             'D' => Some(Move::Down),
@@ -32,37 +34,86 @@ fn file_to_moves (file_name : String) -> Vec<(Move, i32)> {
 }
 
 fn num_tail_locations (moves : Vec<(Move, i32)>) -> i32 {
+    let mut tail_visited : Vec<(i32, i32)> = vec!();
     let mut head = (0,0);
     let mut tail = (0,0);
+    tail_visited.push(tail);
     for (m, n) in moves {
         for _ in 0..n {
-            head = move_head(head, &m);
-            println!("{:?}", head);
+            move_head(&mut head, &m);
+            move_tail(&mut tail, &head);
+            match tail_visited.iter().find(|x| x.0 == tail.0 && x.1 == tail.1) {
+                None => {
+                    tail_visited.push(tail);
+                },
+                Some(_) => {},
+            }
+            // println!("{:?} {:?}", head, tail);
         }
     }
-    return 0;
+    return tail_visited.len() as i32;
 }
 
-fn move_head (head: (i32, i32), m : &Move) -> (i32, i32) {
+fn move_head (head: &mut (i32, i32), m : &Move) {
     match m {
         Move::Up => {
-            return (head.0, head.1 + 1);
+            head.1 = head.1 + 1;
         },
         Move::Down => {
-            return (head.0, head.1 - 1);
+            head.1 = head.1 - 1;
         },
         Move::Right => {
-            return (head.0 + 1, head.1);
+            head.0 = head.0 + 1;
         },
         Move::Left => {
-            return (head.0 - 1, head.1);
+            head.0 = head.0 - 1;
         },
+    }
+}
+
+fn move_tail (tail: &mut (i32,i32), head : &(i32,i32)) {
+    // should move diag if possible
+    let x_diff = head.0 - tail.0;
+    let y_diff = head.1 - tail.1;
+
+    // if x_diff.abs() <= 1 && y_diff.abs() <=1 {
+    //     return;
+    // }
+    // if x_diff == 0 {
+    //     let step = y_diff/y_diff.abs();
+    //     tail.1 = tail.1 + step;
+    // }
+    // else if y_diff == 0 {
+    //     let step = x_diff/x_diff.abs();
+    //     tail.0 = tail.0 + step;
+    // }
+    // else {
+    //     let step = y_diff/y_diff.abs();
+    //     tail.1 = tail.1 + step;
+    //     let step = x_diff/x_diff.abs();
+    //     tail.0 = tail.0 + step;
+    // }
+
+
+    if x_diff.abs() <= 1 && y_diff.abs() <=1 {
+        return;
+    }
+
+    if x_diff != 0 {
+        let step = x_diff/x_diff.abs();
+        tail.0 = tail.0 + step;
+    }
+    if y_diff != 0 {
+        let step = y_diff/y_diff.abs();
+        tail.1 = tail.1 + step;
     }
 }
 
 fn main() {
-    println!("Hello, world!");
-    let moves = file_to_moves(String::from("files/head_movements_test.txt"));
-    println!("{:?}", moves);
-    let _ = num_tail_locations(moves);
+    let moves = file_to_moves(String::from("files/head_movements.txt"));
+    // let moves = file_to_moves(String::from("files/head_movements_test.txt"));
+    // println!("{:?}", moves);
+    println!("{}", moves.len());
+    let part1 = num_tail_locations(moves);
+    println!("Part 1: {}", part1);
 }
