abe1f4cff8cb30d04bcf57af8c9363b87b60a89e
Luca Hodge <lucahodge@gmail.com>
2023-11-09 16:24:32 -0700

    doing the cycling in the wrong way

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,4 @@
+// need to have it be executing cycles
 
 #[derive(Debug)]
 enum Instruction{
@@ -7,16 +8,45 @@ enum Instruction{
 
 struct Register {
     value : i32,
+    executing_instructions : Vec<(i32, Instruction)>,
 }
 
 impl Register {
     fn new(starting_value : i32) -> Register {
-        return Register{value : starting_value};
+        return Register{value : starting_value, executing_instructions : vec!()};
     }
 
-    fn cycle(&self, instruction : Instruction) -> i32 {
-        return 0;
+    fn cycle(&mut self, instruction : Instruction) -> i32 {
+        match instruction {
+            Instruction::Noop => {},
+            Instruction::Addx(_) => {
+                self.executing_instructions.push((2, instruction));
+            }
+        }
 
+        let register_value = self.value;
+
+        let mut index = 0;
+        while index < self.executing_instructions.len() {
+            match &self.executing_instructions[index] {
+                (0, ins) => {
+                    match ins {
+                        Instruction::Addx(val) => {
+                            self.value += val;
+                        },
+                        _ => {},
+                    };
+                    self.executing_instructions.remove(index);
+                },
+                (num, ins) => {
+                    self.executing_instructions[index].0 -= 1;
+                    index += 1;
+                }
+            }
+
+        }
+
+        return register_value;
     }
 }
 
@@ -35,10 +65,22 @@ fn file_to_instuctions(file_name : String) -> Vec<Instruction> {
     return instructions;
 }
 
+fn execute_instructions(register : &mut Register, instructions : Vec<Instruction>) {
+    for (index, instr) in instructions.into_iter().enumerate() {
+        let val = register.cycle(instr);
+        if (index+20)%40 == 0 {
+            println!("{} {}", index, val);
+        }
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
     let instructions = file_to_instuctions(String::from("files/instructions_test.txt"));
-    println!("{:?}", instructions);
+    // println!("{:?}", instructions);
 
-    
+    let mut register = Register::new(1);
+    execute_instructions(&mut register, instructions);
+    println!("{}", register.value);
 }
